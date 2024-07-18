25dadf00f8da73baea6a6b288db6d0010ca77a6d
parnia-mohammadi <parniiiiia.mohammadi@gmail.com>
2024-07-18 23:03:12 +0330

    header edited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 function Header() {
   return (
-    <div className="flex flex-row-reverse">
-      <img src="src/assets/Header/HamburgerMenu.svg" alt="menu" className="self-start"/>
-      <img src="src/assets/Header/Logo.svg" alt="logo"/>
-       <img src="src/assets/Header/CartLogo.svg" alt="cartLogo"/>
-        <img src="src/assets/Header/UserLogo.svg" alt="userLogo"/>
+    <div className="flex flex-row-reverse justify-around items-center px-5 py-4">
+      <img src="src/assets/Header/HamburgerMenu.svg" alt="menu" />
+      <img src="src/assets/Header/Logo.svg" alt="logo" />
+      <div className="flex flex-row-reverse gap-x-1">
+        <div className="bg-green-tint border-none rounded-[4px] p-1">
+          <img src="src/assets/Header/CartLogo.svg" alt="cartLogo" />
+        </div>
+        <div className="bg-green-tint border-none rounded-[4px] p-1">
+          <img src="src/assets/Header/UserLogo.svg" alt="userLogo" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-      'green':{
-        'primary' :'#417F56',
-      }
+    colors: {
+      green: {
+        primary: "#417F56",
+        tint: "#E5F2E9",
+      },
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
